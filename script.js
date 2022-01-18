11c727261939975c7fe75e1464a8cfc77fc01d90
zogrez <60665971+zogrez@users.noreply.github.com>
2022-01-18 16:45:25 +0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const body = document.querySelector("body"),
+    sidebar = body.querySelector(".sidebar"),
+    toogle = body.querySelector(".toogle"),
+    searchBtn = body.querySelector(".search-box"),
+    modeSwitch = body.querySelector(".toogle-switch"),
+    modeText = body.querySelector(".mode-text");
+
+    toogle.addEventListener("click", () => {
+        sidebar.classList.toggle("close");
+    });
+
+    searchBtn.addEventListener("click", () => {
+        sidebar.classList.remove("close");
+    });
+
+    modeSwitch.addEventListener("click", () => {
+        body.classList.toggle("dark");
+
+        if(body.classList.contains("dark")){
+            modeText.innerText = "Light Mode"
+        } else {
+            modeText.innerText = "Dark Mode"
+        }
+    });

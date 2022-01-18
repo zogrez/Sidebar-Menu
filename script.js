0e29ee26e4b67b61809eb6c2900c29d1943043fe
zogrez <60665971+zogrez@users.noreply.github.com>
2022-01-18 16:49:52 +0700

    Initial Commit

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const body = document.querySelector("body"),
     modeText = body.querySelector(".mode-text");
 
     toogle.addEventListener("click", () => {
-        sidebar.classList.toggle("close");
+        sidebar.classList.toggle("close"); 
     });
 
     searchBtn.addEventListener("click", () => {
